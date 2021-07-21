2e4ba7529184b0390087783889dea6d1d9f77037
Shawn Wagner <shawnw.mobile@gmail.com>
2021-07-20 19:54:29 -0700

    Add line reading and splitting routines.

--- recolumn.cpp
+++ recolumn.cpp
@@ -21,9 +21,20 @@
  * SOFTWARE.
  */
 
+#include <memory>
 #include <iostream>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+#include <unicode/regex.h>
+#include <unicode/ucnv.h>
+#include <unicode/unistr.h>
+#include <unicode/ustdio.h>
 
 #include <getopt.h>
+
+using namespace std::literals::string_literals;
 
 const char *version = "0.1";
 
@@ -35,6 +46,72 @@ void print_usage(const char *name) {
       << " -v/--version\t\tDisplay version.\n"
       << " -d/--delimiter=RE\tSet the column separator regular expression.\n"
       << " -c/--colspec=SPEC\tSet the column specification.\n";
+}
+
+using colvector = std::vector<icu::UnicodeString>;
+
+class line_breaker {
+private:
+  std::unique_ptr<icu::RegexPattern> pattern;
+  std::unique_ptr<icu::RegexMatcher> splitter;
+  colvector fields;
+  bool getline(UFILE *, icu::UnicodeString *out);
+
+public:
+  line_breaker(const icu::UnicodeString &re);
+  bool split(UFILE *, colvector *out);
+};
+
+line_breaker::line_breaker(const icu::UnicodeString &re) {
+  UParseError pe;
+  UErrorCode err = U_ZERO_ERROR;
+  pattern = std::unique_ptr<icu::RegexPattern>(
+      icu::RegexPattern::compile(re, pe, err));
+  if (U_FAILURE(err)) {
+    throw std::invalid_argument{"Invalid regular expression: "s +
+                                u_errorName(err)};
+  }
+  splitter = std::unique_ptr<icu::RegexMatcher>(pattern->matcher(err));
+  if (U_FAILURE(err)) {
+    throw std::runtime_error{"Couldn't create RegexMatcher: "s +
+                             u_errorName(err)};
+  }
+}
+
+bool line_breaker::getline(UFILE *uf, icu::UnicodeString *out) {
+  UChar buffer[4096];
+  out->remove();
+  do {
+    UChar *s = u_fgets(buffer, 4096, uf);
+    if (!s) {
+      return u_feof(uf) && !out->isEmpty();
+    }
+    out->append(s, -1);
+    auto lastidx = out->length() - 1;
+    if (out->charAt(lastidx) == '\n') {
+      out->remove(lastidx);
+      return true;
+    }
+  } while (1);
+}
+
+bool line_breaker::split(UFILE *uf, colvector *out) {
+  icu::UnicodeString line;
+  UErrorCode err = U_ZERO_ERROR;
+
+  if (!getline(uf, &line)) {
+    return false;
+  }
+
+  if (fields.size() < line.length()) {
+    fields.resize(line.length());
+  }
+  auto nfields = splitter->split(line, &fields[0], fields.size(), err);
+  if (U_FAILURE(err)) {
+    return false;
+  }
+  out->assign(fields.begin(), fields.begin() + nfields);
+  return true;
 }
 
 int main(int argc, char **argv) {
@@ -65,6 +142,34 @@ int main(int argc, char **argv) {
     default:
       return 1;
     }
+  }
+
+  try {
+    UErrorCode err = U_ZERO_ERROR;
+
+    icu::UnicodeString usplit_re{split_re, -1, nullptr, err};
+    if (U_FAILURE(err)) {
+      throw std::runtime_error{"Couldn't convert '"s + split_re +
+                               "' to unicode: "s + u_errorName(err)};
+    }
+
+    line_breaker breaker{usplit_re};
+    colvector fields;
+
+    std::unique_ptr<UFILE, decltype(&u_fclose)> ustdin{
+        u_fadopt(stdin, nullptr, nullptr), &u_fclose};
+    if (!ustdin) {
+      throw std::runtime_error{"Unable to read from standard input"};
+    }
+
+    int line = 0;
+    while (breaker.split(ustdin.get(), &fields)) {
+      u_printf("Line %d has %d fields\n", ++line, (int)fields.size());
+    }
+
+  } catch (std::exception &e) {
+    std::cerr << "Error: " << e.what() << '\n';
+    return 1;
   }
 
   return 0;
